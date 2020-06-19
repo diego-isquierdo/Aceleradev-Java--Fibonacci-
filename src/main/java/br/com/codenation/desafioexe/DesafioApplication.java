25e4ff36b737144c50ac71bcc962accfb9dd3616
Diego Isquierdo <49922920+diego-isquierdo@users.noreply.github.com>
2020-06-19 08:22:25 -0300

    Refatorado Código
    Substituído while por for e alterado criação do List fibonacciList

--- src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -4,15 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DesafioApplication {
-
 	public static List<Integer> fibonacci() {
-		List<Integer> fibonacciList = new ArrayList<>();
-		fibonacciList.add(0);
-		fibonacciList.add(1);
+		List<Integer> fibonacciList = new ArrayList<>(Arrays.asList(0,1));
 
-		int i = 1;
-		while (i < 350) {
-			i = Integer.sum(fibonacciList.get(fibonacciList.size() - 1), fibonacciList.get(fibonacciList.size() - 2));
+		for (int i=1; i<350;){
+			i = Integer.sum(fibonacciList.get(fibonacciList.size()-2),
+					fibonacciList.get(fibonacciList.size() - 1));
+
 			fibonacciList.add(i);
 		}
 		return fibonacciList;
@@ -21,5 +19,4 @@ public class DesafioApplication {
 	public static Boolean isFibonacci(Integer a) {
 		return fibonacci().contains(a);
 	}
-
 }
